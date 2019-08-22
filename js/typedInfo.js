3749ec254f1dfaba711e29cc413cc3eaa0dbe1ae
Benny Borunda <blznk128@yahoo.com>
2019-08-22 11:58:57 -0600

    added address book

--- js/typedInfo.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed(".display-4", {
-    strings: [
-        " honesty", " integrity", " hard work", " passion"
-    ],
-    typeSpeed: 150,
-    backSpeed: 50,
-    loop: true
-  
-  });
